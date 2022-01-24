0b613ad5814131728efd5b9be6f7d62ccc7cf290
Purnima-1 <baranwal.purnima@gmail.com>
2022-01-24 00:30:10 -0800

    second

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,7 +20,7 @@ export class NavBar extends Component {
                                     <a className="nav-link active" aria-current="page" href="/">Home</a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <a className="nav-link" href="/about">About Us</a>
                                     {/* <Link to="/about">About</Link> */}
                                 </li>
                                 
